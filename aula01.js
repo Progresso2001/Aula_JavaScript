8dc13be8f2a75f5445920023d4423b2bf2d381a0
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-02 23:16:18 +0530

    modo estrito

---
+++ aula01.js
@@ -0,0 +1,11 @@
+/*
+Modo estrito(Strict Mode): é uma ferramenta que 
+permite evitar erros comuns e comportamentos silenciosos
+ele é ativado usando "use strict"
+*/
+"use strict" 
+// Nota: excluir a variavel let para notar o modo estrito
+let nome = "Joaquim"
+console.log("DevJocas")
+console.log(nome)
+console.log("Nome: " + nome)
