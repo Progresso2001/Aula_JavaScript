803a320ad729b1d32787a685c225e0becd66f18f
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-03 00:08:33 +0530

    usando variaveis

---
+++ aula02.js
@@ -0,0 +1,23 @@
+// Variaveis e suas diferencias
+"use strict"
+
+let nome = "Joaquim"
+console.log(nome)
+var nome = "jp"
+console.log(nome)
+let nome = "Hadassa"   // A variavel ira imprimir o seu valor por estar fora do escopo
+if (true){
+    // let nome = "Rosa"  // A variavel não  ira imprimir por estar dentro do escopo
+    var x = "Joaquim"
+}
+console.log("O seu meu é " + x)
+
+const num = 35  //A variavel const é um tipo de variavel que não permite a mudança ou alteração de valor na memoria do pc.
+
+
+num = 10  //Ela vai gerar um erro do tipo TypeError: Assignment to constant variable, porque não se pode alterar o valor da variavel const
+
+// usando o let para alterar o const
+
+let num = 50
+console.log("Tens " +  num + " anos de idade")
