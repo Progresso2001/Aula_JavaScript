1b3ba9a4ee474e3579fb3e217131a8569eb016eb
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-02 22:19:21 +0530

    commit inicial

---
+++ aula.js
@@ -0,0 +1,7 @@
+// Este é um copmentario de uma linha em Js
+/*
+Este é um comentario para varias linhas 
+*/
+console.log("Olá Joaquim Seja bem-vindo ao estudo do JavaScript");
+console.log("Tu vais aprender a programar JS")
+console.log("Seja Resiliente")
