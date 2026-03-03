4b32d899c71e488db841879cc9453f39bb1e6b7f
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-03 20:53:10 +0530

    operadores matemáticos

---
+++ aula05.js
@@ -0,0 +1,30 @@
+// operadores matematicos
+// tipos de operadores 
+/*
+Operadores Básicos
+Estes executam as operações fundamentais da matemática:
++ (adição): Soma dois valores, como 5 + 3 resulta em 8.
+- (subtração): Subtrai o segundo valor do primeiro, como 5 - 3 resulta em 2.
+* (multiplicação): Multiplica valores, como 5 * 3 resulta em 15.
+/ (divisão): Divide o primeiro pelo segundo, como 6 / 2 resulta em 3.
+
+Operadores de Resto e Potência
+% (módulo ou resto): Retorna o resto da divisão, como 10 % 3 resulta em 1.
+** (exponenciação): Eleva à potência, como 2 ** 3 resulta em 8.
+Operadores Unários
+São aplicados a um único operando:
+++ (incremento): Aumenta o valor em 1 (x++ ou ++x).
+-- (decremento): Diminui o valor em 1 (x-- ou --x).
++ (unário positivo): Converte para número positivo.
+- (unário negativo): Converte para número negativo.
+*/
+
+let num1=0, num2=0, resul=0
+num1=10
+num2=6
+resul=num1+num2
+resul=num1/num2
+resul=num1%num2
+resul=num1*num2
+console.log(resul)
+console.log((num1-num2)**3)
