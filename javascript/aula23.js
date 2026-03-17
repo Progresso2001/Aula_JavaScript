d30ed21b88302af9bd31c67344368b1434fe742a
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-17 22:00:03 +0530

    atualizado

--- javascript/aula23.js
+++ javascript/aula23.js
@@ -1,6 +1,7 @@
-// Expressões Regulares
-const re = /ab+c/
-console.log(re)
-// usando a funcao construtora RegExp
-const ex = new RegExp("ab+c") 
-console.log(ex)
+// // Expressões Regulares
+// const re = /ab+c/
+// console.log(re)
+// // usando a funcao construtora RegExp
+// const ex = new RegExp("ab+c") 
+// console.log(ex)
+
