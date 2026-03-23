db328551f1cff0ac32b337da8a0a2a2509d1cfc1
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-23 22:58:29 +0530

    estudo de array

---
+++ javascript/aula24.js
@@ -0,0 +1,21 @@
+// ARRAY
+// var arr = [];
+// arr.length = 45;
+// console.log(arr)
+
+// var arr = Array(76.8)
+// console.log(arr)
+// Povoando um Array
+
+var empregados = []
+empregados[0] = "Ricardo";
+empregados[1] = "Bento";
+empregados[2] = "Rosa";
+empregados[3] = "Amoroso";
+// console.log(empregados)
+console.log(empregados[1])
+
+// var vazio = []
+// vazio[3.6] = 'Banana';
+// console.log(vazio)
+// console.log(vazio.hasOwnProperty(3.6))
