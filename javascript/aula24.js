59977b1a50457f3afeaf60373ddcc6bfcb2c70c1
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-24 03:24:36 +0530

    usando a indexação e o metodo push

--- javascript/aula24.js
+++ javascript/aula24.js
@@ -7,15 +7,28 @@
 // console.log(arr)
 // Povoando um Array
 
-var empregados = []
-empregados[0] = "Ricardo";
-empregados[1] = "Bento";
-empregados[2] = "Rosa";
-empregados[3] = "Amoroso";
+// var empregados = []
+// empregados[0] = "Ricardo";
+// empregados[1] = "Bento";
+// empregados[2] = "Rosa";
+// empregados[3] = "Amoroso";
 // console.log(empregados)
-console.log(empregados[1])
+// console.log(empregados[1])
 
 // var vazio = []
 // vazio[3.6] = 'Banana';
 // console.log(vazio)
 // console.log(vazio.hasOwnProperty(3.6))
+
+const listAlimento = [
+    "Pão",
+    "Peixe",
+    "Feijão"
+]
+listAlimento[2] = "Macarão";
+console.log(listAlimento.push("Frango"))
+console.log("Listas de alimentos favouritos: " + listAlimento)
+console.log("Quantidade alimentos: " + listAlimento.length)
+console.log("Eu gosto de: " + listAlimento[2] + ", " + `acompanhado de ${listAlimento[1]}`)
+listAlimento.push("Carne de Boi"); // O metodo push() usado para adicionar novo elemento dentro do array
+console.log("Adicionado novo alimento: " + listAlimento[3])
