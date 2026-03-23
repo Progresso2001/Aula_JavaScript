177876dae5ab363c2a2a2df9de6d2403b118ba73
Progresso2001 <joaquimmenianga99@gmail.com>
2026-03-24 03:29:18 +0530

    criando array vazio

--- javascript/aula24.js
+++ javascript/aula24.js
@@ -20,15 +20,25 @@
 // console.log(vazio)
 // console.log(vazio.hasOwnProperty(3.6))
 
-const listAlimento = [
-    "Pão",
-    "Peixe",
-    "Feijão"
-]
-listAlimento[2] = "Macarão";
-console.log(listAlimento.push("Frango"))
-console.log("Listas de alimentos favouritos: " + listAlimento)
-console.log("Quantidade alimentos: " + listAlimento.length)
-console.log("Eu gosto de: " + listAlimento[2] + ", " + `acompanhado de ${listAlimento[1]}`)
-listAlimento.push("Carne de Boi"); // O metodo push() usado para adicionar novo elemento dentro do array
-console.log("Adicionado novo alimento: " + listAlimento[3])
+// const listAlimento = [
+//     "Pão",
+//     "Peixe",
+//     "Feijão"
+// ]
+// listAlimento[2] = "Macarão";
+// console.log(listAlimento.push("Frango"))
+// console.log("Listas de alimentos favouritos: " + listAlimento)
+// console.log("Quantidade alimentos: " + listAlimento.length)
+// console.log("Eu gosto de: " + listAlimento[2] + ", " + `acompanhado de ${listAlimento[1]}`)
+// listAlimento.push("Carne de Boi"); // O metodo push() usado para adicionar novo elemento dentro do array
+// console.log("Adicionado novo alimento: " + listAlimento[3])
+
+var meuArray = []
+var minhaIdentidade = {
+    nome:"Joaquim",
+    idade:25,
+    profissao: "Programador Iniciante"
+}
+
+meuArray.push(minhaIdentidade)
+console.log(meuArray)
