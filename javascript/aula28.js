bd33d8965f334cf27b65b6c17d66acb212160695
Progresso2001 <joaquimmenianga99@gmail.com>
2026-04-08 18:51:28 +0530

    usando propriendades

--- javascript/aula28.js
+++ javascript/aula28.js
@@ -36,8 +36,10 @@
 // Acessar a propriedades usando valor da string que esta armazenda em uma variavel
 
 // var meuCarro = new Object()
-// var nomeDaPropriedade = "Fabricacao";
+// var nomeDaPropriedade = "fabricacao";
 // meuCarro[nomeDaPropriedade] = "Ford"
+
+// console.log(meuCarro["fabricacao"])
 
 // var nomeDaPropriedade = "Modelo"
 // meuCarro[nomeDaPropriedade] = "Mustang"
@@ -72,27 +74,88 @@
 
 //USANDO PROPRIDADES ENUMERAVEIS
 
-function listarTodasAsPropriedades(o) {
-  var objectoASerInspecionado;
-  var resultado = [];
+// function listarTodasAsPropriedades(o) {
+//   var objectoASerInspecionado;
+//   var resultado = [];
 
-  for (
-    objectoASerInspecionado = o;
-    objectoASerInspecionado !== null;
-    objectoASerInspecionado = Object.getPrototypeOf(objectoASerInspecionado)
-  ) {
-    resultado = resultado.concat(
-      Object.getOwnPropertyNames(objectoASerInspecionado),
-    );
-  }
+//   for (
+//     objectoASerInspecionado = o;
+//     objectoASerInspecionado !== null;
+//     objectoASerInspecionado = Object.getPrototypeOf(objectoASerInspecionado)
+//   ) {
+//     resultado = resultado.concat(
+//       Object.getOwnPropertyNames(objectoASerInspecionado),
+//     );
+//   }
 
-  return resultado;
-}
+//   return resultado;
+// }
 
-const pessoa = {
-    nome: "Progresso",
-    idade: 33,
-    email: "progresso@yandex.com"
-}
+// const pessoa = {
+//     nome: "Progresso",
+//     idade: 33,
+//     email: "progresso@yandex.com"
+// }
 
-console.log(listarTodasAsPropriedades(pessoa))
+// console.log(listarTodasAsPropriedades(pessoa))
+
+// usando as prop: Object.defineProperty e propertyIsEnumerable()
+
+// const obj ={a: 1, b: 2}
+// Object.defineProperty(obj, 'c', { value: 3, enumerable:false});
+// for(let prop in obj){
+//   console.log(prop)
+// }
+// console.log(obj.propertyIsEnumerable('c'))
+
+// usando o Object.keys()
+
+// const pessoa = {nome:"Joaquim", idade: 45, cidade: "Luanda"}
+// const chaves = Object.keys(pessoa)
+// console.log(chaves)
+
+// iterando
+// const pessoa = {nome:"Joaquim", idade: 45, cidade: "Luanda"}
+// Object.keys(pessoa).forEach(chaves => { //usando o metodo map() e forEach()
+//   console.log(`${chaves}: ${pessoa[chaves]}`)
+// })
+
+// usando o Object.getOwnPropertyNames
+
+// const usuario ={nome:'Maria', idade: 22, email:"maria@gamil.com"}
+// // Adicionar prop nao enumeraveis
+
+// Object.defineProperty(usuario, 'senha', {
+//   value:"1234456",
+//   enumerable: false,
+//   writable:true
+// });
+// console.log(Object.keys(usuario))
+// console.log(Object.getOwnPropertyNames(usuario))
+
+// Exemplos usando o Object.defineProperty
+const carro = []
+
+Object.defineProperty(carro, 'marca', {
+  value:'Toyota',
+  writable:false, //Nao pode ser alterado
+  enumerable: true, //Aparece no obj.key()
+  configurable: false, //Nao pode deletar(apagar)
+});
+carro.marca = 'Honda', //Falha silenciosamente
+console.log("O valor principal da marca de carro é: " + carro.marca) 
+console.log(Object.keys(carro)) 
+
+// Object.defineProperty: usando getter e setter
+
+let idade =  0;
+Object.defineProperty(idade, 'anos', {
+  get() { return idade; },
+  set(valor) { idade = valor; },
+  enumerable :true
+});
+
+carro.anos = 5;
+console.log(carro.anos);
+
+
