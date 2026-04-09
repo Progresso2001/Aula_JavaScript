9debc92e7cde91421f18f3b2f9d7c88e358bc992
Progresso2001 <joaquimmenianga99@gmail.com>
2026-04-09 17:51:31 +0530

    novo exemplo

--- javascript/aula30.js
+++ javascript/aula30.js
@@ -1,44 +1,56 @@
-// Usando o método Object.create
+// // Usando o método Object.create
 
-var Animal ={
-    tipo: "Invertebrados", // Prop de valores padrao
-    qualTipo: function(){
-        // Método que irá mostrar o tipo de animal
-       console.log( this.tipo)
-    },
-};
-// Cria um novo tipo de animal chamado animal1
+// var Animal ={
+//     tipo: "Invertebrados", // Prop de valores padrao
+//     qualTipo: function(){
+//         // Método que irá mostrar o tipo de animal
+//        console.log( this.tipo)
+//     },
+// };
+// // Cria um novo tipo de animal chamado animal1
 
-var animal1 = Object.create(Animal)
-animal1.qualTipo(); 
+// var animal1 = Object.create(Animal)
+// animal1.qualTipo(); 
 
-// Cria um outrotipo de animal chamado peixe
+// // Cria um outrotipo de animal chamado peixe
 
-var peixe = Object.create(Animal);
-peixe.tipo = "Peixes";
-peixe.qualTipo()
+// var peixe = Object.create(Animal);
+// peixe.tipo = "Peixes";
+// peixe.qualTipo()
 
-const humano = {
-  saudar() {
-    console.log(`Olá, eu sou ${this.nome}`);
+// const humano = {
+//   saudar() {
+//     console.log(`Olá, eu sou ${this.nome}`);
+//   }
+// };
+
+// const pessoa = Object.create(humano, {
+//   nome: {
+//     value: "João",
+//     writable: true,
+//     enumerable: true
+//   }
+// });
+
+// pessoa.saudar(); 
+
+// const animal = {
+//     falar(){
+//         console.log("Faz algum som")
+//     }
+// };
+// const cachorro = Object.create(animal);
+// cachorro.falar()
+
+const tel = {
+  chamar(tipo, nome){
+    this.tipo = tipo,
+    this.nome = nome
+    console.log(`O ${this.tipo} esta chamando , alguém atende o ${this.nome}?`)
   }
+
 };
+const m = Object.create(tel)
 
-const pessoa = Object.create(humano, {
-  nome: {
-    value: "João",
-    writable: true,
-    enumerable: true
-  }
-});
-
-pessoa.saudar(); 
-
-const animal = {
-    falar(){
-        console.log("Faz algum som")
-    }
-};
-const cachorro = Object.create(animal);
-cachorro.falar()
+m.chamar("Iphone", "joaquim")
 
